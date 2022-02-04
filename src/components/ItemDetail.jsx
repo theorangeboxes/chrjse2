d5a813f71ebaaaa9f3e3c1e671042b61394dcabb
Matias Alvarez <theorangeboxes@gmail.com>
2022-02-04 09:29:38 -0300

    Feature: Responsive ItemDetails

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -4,12 +4,12 @@ import ItemCount from "./ItemCount";
 const ItemDetail = ({ item }) => {
   return (
     <>
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col text-center'>
-            <img src={item.urlImg} className='img-fluid' alt='' />
+       <div className='container-fluid'>
+        <div className='d-flex flex-wrap justify-content-center'>
+          <div className='d-flex' style={{width:'600px'}}>
+            <img src={item.urlImg} className='rounded' style={{width:'100%', margin: '0px 70px 0px 0px'}} alt='' />
           </div>
-          <div className='d-flex flex-column col text-left'>
+          <div className='d-flex flex-column' style={{width:'600px'}}>
             <h1>{item.name}</h1>
             <h2>{`$ ${item.cost}`}</h2>
             <p>{item.details}</p>
